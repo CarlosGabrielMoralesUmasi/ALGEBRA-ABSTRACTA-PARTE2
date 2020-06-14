a907da17abfceb37bfa6273a8d75cb4b95f89290
Carlos Gabriel Morales Umasi <52215770+CarlosGabrielMoralesUmasi@users.noreply.github.com>
2020-06-14 10:54:39 -0500

    Update AFIN_CIF.cpp

--- CIFRADO AFIN/AFIN_CIF.cpp
+++ CIFRADO AFIN/AFIN_CIF.cpp
@@ -1,184 +1,119 @@
 #include <iostream>
+#include <string>
 #include <stdlib.h>
 using namespace std;
-class afin{
 
-    public:
-        afin();
-        afin(int,int);
-        int clave1,clave2;
-        string cifrar(string);
-        string descifrar(string,int,int);
-        string alfabeto;
-        int inversa(int,int);
-        int generar_clave_a();
-        int generar_clave_b();
-        int resto(int ,int );
-        int euclides(int,int);
-        int obtener_clave_a();
-        int obtener_clave_b();
+class Cif_Afin
+{
+  public:
+  string alfabeto;
+  int c,zn,c2;
+
+  Cif_Afin(int _c);
+  int mod(int x,int y);
+  int mcd(int x, int y);
+  int euclidesext(int x,int y);
+  int inversa(int x,int y);
+  string cifrar(string msj);
+  string decifrar(string cmsj);
 };
-afin::afin(){
-    alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
-}
-afin::afin(int clave_a,int clave_b){
-    alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-    clave_a=generar_clave_a();
-    clave_b=generar_clave_b();
-    clave1=clave_a;
-    clave2=clave_b;
-}
-
-int afin::obtener_clave_a()
+Cif_Afin::Cif_Afin(int _c)
 {
-    return clave1;
+  alfabeto="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+  c=_c;
+  zn=alfabeto.length();
+  c2=mod(rand() % 100,zn);
 }
-int afin::obtener_clave_b()
-{
-    return clave2;
+int Cif_Afin::mod(int x,int y)
+{int r;
+  r=x-((x/y)*y);
+  if(r<0)
+    r=x-(((x/y)-1)*y);
+  return r; 
 }
-int afin::generar_clave_a()
-{
-    srand(time(NULL));
-    int clave_a,tam;
-    tam=alfabeto.length();
-    do
-    {
-        clave_a=1+rand()%(1001-1);
-
-    }
-    while(euclides(clave_a,tam)!=1);
-
-    return clave_a;
+int Cif_Afin::mcd(int x,int y)
+{int r;
+  r=mod(x,y);
+  while (r > 0)
+  { x=y;
+    y=r;
+    r = mod(x,y);
+  }
+return y;
 }
-int afin::generar_clave_b()
-{
-    int clave_b,tam;
-    tam=alfabeto.length();
-    clave_b=1+rand()%(1001-1);
-    clave_b=resto(clave_b,tam);
-
-    return clave_b;
-
-}
-int afin::euclides(int a, int b)
-{
-    int r;
-    do
-    {
-        r=resto(a,b);
-        a=b;
-        b=r;
-    }
-    while(r!=0);
-
-    return a;
-
-}
-int afin::resto(int a,int b)
-{
-        int r;
-        r=a-((a/b)*b);
-        if(r<0)
-        {
-            r=b-r;
-        }
-        return r;
-}
-string afin::cifrar(string mensaje)
-{
-    int tam=alfabeto.length();
-    int pos_letra,pos_letra_cifrada;
-    string palabra_cifrada;
-
-    for(int i=0;i<mensaje.length();i++)
-    {
-        pos_letra=alfabeto.find(mensaje[i]);
-        pos_letra_cifrada=((pos_letra*clave1)+clave2)%tam;
-        palabra_cifrada=palabra_cifrada+alfabeto.at(pos_letra_cifrada);
-    }
-    cout<<palabra_cifrada;
-    return palabra_cifrada;
-
-}
-
-string afin::descifrar(string mensaje,int clave_1,int clave_2){
-    cout<<"\nLAS CLAVES GENERADAS FUERON: "<<clave_1<<" y "<<clave_2<<" ";
-    int tam=alfabeto.length();
-    int pos_letra_cifrada,pos_letra_descifrada;
-    string palabra_descifrada;
-    clave_1=inversa(clave_1,tam);
-    for(int i=0;i<mensaje.length();i++)
-    {
-        pos_letra_cifrada=alfabeto.find(mensaje[i]);
-        pos_letra_descifrada=(clave_1*(pos_letra_cifrada-clave_2))%(tam);
-        if(pos_letra_descifrada<0)
-        {
-            pos_letra_descifrada=pos_letra_descifrada+tam;
-        }
-
-        palabra_descifrada=palabra_descifrada+alfabeto.at(pos_letra_descifrada);
-    }
-    cout<<palabra_descifrada;
-    return palabra_descifrada;
-}
-int afin::inversa(int a, int b)
-{
-	int mod=b;
-	int mcd = 0;
-
-	int r1 = a, r2 = b;
-	int s1 = 1, s2 = 0;
-	int t1 = 0,	t2 = 1;
-	int s;
-	int t;
-	while (r2 > 0)
-	{
-		int q = r1 / r2;
-
-		int r = r1 - (q * r2);
-		r1 = r2; r2 = r;
-
-		s = s1 - (q * s2);
-		s1 = s2; s2 = s;
-
-		t = t1 - (q * t2);
-		t1 = t2; t2 = t;
-	};
-
-	s = s1;
-	t = t1;
-	mcd = r1;
-
-	if(mcd ==1)
+int Cif_Afin::euclidesext(int x,int y)
+{int q,s,t,r;
+  int r1=x,r2=y,s1=1,s2=0,t1=0,t2=1;
+  while(r2>0)
   {
-        if(s<0)
-        {
-        s=s+b;
-        }
-        return s;
-	}
-	else 
+    q=r1/r2;
+    r=r1-q*r2;
+    r1=r2;
+    r2=r;
+
+    s=s1-q*s2;
+    s1=s2;
+    s2=s;
+
+    t=t1-q*t2;
+    t1=t2;
+    t2=t;
+  }
+  s=s1;
+  t=t1;
+return s;
+}
+int Cif_Afin::inversa(int x,int y)
+{
+  if(mcd(x,y)==1)
   {
-        return 0;
-	}
+    int inv=euclidesext(x,y);
+    if(mod((inv*x),y)==1)
+    {
+      if (inv<0)
+        inv=mod(inv,y);
+      return inv;
+    }
+  }else
+    return 0;
+return 0;
+}
+string Cif_Afin::cifrar(string msj)
+{int aux,aux2;
+  string cmsj;
+  for(int i=0;i<msj.length();i++)
+  {
+    aux=alfabeto.find(msj[i]);
+    aux2=mod(((c*aux)+c2),zn);
+    cmsj+=alfabeto[aux2];
+  }
+return cmsj;
+}
+
+string Cif_Afin::decifrar(string cmsj)
+{int aux,aux2,aux3;
+  string dmsj;
+  aux=inversa(c,zn);
+  if(!aux)
+    return 0;
+  for(int i=0;i<cmsj.length();i++)
+  {
+    aux2=alfabeto.find(cmsj[i]);
+    aux3=mod(aux*(aux2-c2),zn);
+    dmsj+=alfabeto[aux3];
+  }
+return dmsj;
 }
 int main()
 {
-
-
-    string mensaje,mensaje_cifrado,mensaje_descifrado;
-    int clave_a,clave_b;
-    afin emisor(clave_a,clave_b);
-
-    cout<<"claves:"<<clave_a<<" "<<clave_b;
-    afin receptor;
-    cout<<"\nINGRESE UNA PALABRA PARA CIFRARLA: ";
-    getline(cin,mensaje);
-    cout<<"\nPALABRA CIFRADA: ";
-    mensaje_cifrado=emisor.cifrar(mensaje);
-    cout<<"\nMENSAJE DESCIFRADO: ";
-    mensaje_descifrado=receptor.descifrar(mensaje_cifrado,emisor.obtener_clave_a(),emisor.obtener_clave_b());
-    return 0;
+  Cif_Afin Cif_Afin(23);
+  string msj;
+  cout<<"ESCRIBE MENSAJE: "<<endl;
+  getline(cin,msj);
+  //string cmsg=Cif_Afin.cifrar(msj);
+  string dmsg=Cif_Afin.decifrar("BAMC");
+  cout<<Cif_Afin.c2<<endl;
+ // cout<<cmsg<<endl;
+  cout<<dmsg<<endl;
 }
